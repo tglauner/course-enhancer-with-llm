5601c9fc95ce0a9d88eb47ed372a6dea3982278f
Tim Glauner <glaunertim@gmail.com>
2024-03-26 22:32:03 -0400

    Excellent quality results by description and what students learn

--- batch_across_all_lectures.py
+++ batch_across_all_lectures.py
@@ -1,5 +1,9 @@
+# batch_across_all_lectures.py
+# Executes command you need to define in loop of json file
 import json
 import subprocess
+
+COMMAND = 'generate_description_based_on_latex.py'
 
 # Load the JSON data from the file
 with open('class_codes.json', 'r') as file:
@@ -8,7 +12,7 @@ with open('class_codes.json', 'r') as file:
 # Loop over the elements and call app.py for each one
 for element in data['elements']:
     # Construct the command to call app.py with the current element as an argument
-    command = ['python', 'generate_quiz_based_on_latex.py', element]
+    command = ['python', COMMAND, element]
     
     # Execute the command
     print('Executing ', command)
--- generate_description_based_on_latex.py
+++ generate_description_based_on_latex.py
@@ -1,7 +1,9 @@
 # generate_description_based_on_latex.py
-# Create description based on latex file
+# Create description based on latex files
+# GPT4 recommends to use pdf file with agent but it's much more expensive
 # Input is the 3 digit lecture number
 # Output is file with description
+# Creates consistent format and wording across all lectures when executed in batch - not sure why it's so good
 
 import os, sys
 from dotenv import load_dotenv
@@ -12,8 +14,12 @@ COURSE_PATH = (
   'C:\\Users\\Dad\\Dropbox\\2 - TG Investments and Research\\Projects\\'
   'Capital Markets text book and course 2023\\Presentations\\'
 )
+DESCRIPTION_PATH = (
+  'C:\\Users\\Dad\\Dropbox\\2 - TG Investments and Research\\Projects\\'
+  'Capital Markets text book and course 2023\\Presentations\\LLM_Descriptions\\'
+)
 MODEL = "gpt-4-1106-preview"
-MAX_TOKENS = 1200
+MAX_TOKENS = 4000
 
 def find_class_name(class_code: str) -> str:
       # Check if the directory exists
@@ -37,11 +43,13 @@ def generate_description_for_section(inputContent: str) -> str:
   prompt = 'You are a graduate school professor and create an online course that is being sold.'
   prompt += 'Write the answer to the following \"What will students be able to do at the end of this section?\"'
   prompt += 'Write in a straightforward style without metaphors or elaborate language.'
+  prompt += 'Provide 5-10 items depending on complexity of lecture. The more complex the more items.'
   prompt += 'Your objective is to maximize the number of students the course is being sold to.'
   prompt += 'Do not exceed 200 character length'
   prompt += 'The content given to you at the end is latex source code of the presentation for the lecture.'
   prompt += 'You are given one lecture only of the course. It is one section or the complete beamer presentation'
   prompt += 'The course content is a finance course for graduate or advanced undergraduate students or professionals who want to work for a bank.' 
+  prompt += 'To avoid hitting your token limit limit the reponse to 800 tokens'
   prompt += 'The latex beamer section of the presentation is here: ' + inputContent
 
   # print('Calling ', MODEL, ' for creating narrative for section')
@@ -74,7 +82,7 @@ def my_function(class_code):
   with open(latex_file_name, 'r', encoding='utf-8') as file:
     content = file.read()
 
-  output = 'Description generated for ' + class_name + '\n\n'
+  output = '****** ------ Description generated for ' + class_name + '\n\n'
 
   # openai API setup
   load_dotenv()
@@ -83,19 +91,18 @@ def my_function(class_code):
   print("Estimated token length of complete program input:", tg_tools.estimate_token_length(content))
 
   # call llm in chunks of text
-  counter = 0
-  sections = tg_tools.extract_sections_from_beamer_latex(content)
-  for section in sections:
-    section_name = section['section_name']
-    print('Processing ', section_name)
-    section_content = section['section_content']
-    section_description = generate_description_for_section(section_content)
-    output += f"************ SECTION: {section_name}\n{section_description}\n\n"
+  counter = 1
+  segments = tg_tools.split_text_by_tokens(content, MAX_TOKENS)
+  for segment in segments:
+    print('Processing segment number ', counter)
+    section_description = generate_description_for_section(segment)
+    output += f"*** --- SEGMENT: {counter}\n{section_description}\n\n"
+    counter = counter + 1
 
   #  print narrative to file
-  description_file_name = os.path.join(COURSE_PATH, class_name, class_code + '_description' + '.txt')
+  description_file_name = os.path.join(DESCRIPTION_PATH, class_code + '_description' + '.txt')
   print('Writing to ', description_file_name)
-  with open(description_file_name, 'w', encoding='utf-8') as file:
+  with open(description_file_name, 'a', encoding='utf-8') as file:
     file.write(output)
         
 if __name__ == '__main__':
